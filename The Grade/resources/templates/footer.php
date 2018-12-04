ab7e15370d0f784dc58c11f30bb641c171e36c55
Jan Rueger <jrueger@online.gibz.ch>
2018-12-04 23:36:30 +0100

    Delete redundant files

--- The Grade/resources/templates/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="container shadow mt-3 p-2 d-flex justify-content-center">
-    <small class="p-1 m-0">&copy; The Grade - Developed by Jan Rueger</small>
-</footer>    
-</body>
-</html>
